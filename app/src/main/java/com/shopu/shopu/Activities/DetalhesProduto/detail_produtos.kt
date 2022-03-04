a678957d0f0d78db13d62e3fe07feb5b909c6f76
samue <Samuka36>
2022-03-04 02:08:54 -0300

    Feita as açoes de vincular os tenis do banco de dados da lista do fire base na pagina de detalhes produto no mesmo colocoquei todos os radios para selecionar os tamanho do tenis se for selecionado alterança de cor com bordas pretas Projeto 72% proximo passo tela de pagamentos

--- app/src/main/java/com/shopu/shopu/Activities/DetalhesProduto/detail_produtos.kt
+++ app/src/main/java/com/shopu/shopu/Activities/DetalhesProduto/detail_produtos.kt
@@ -2,11 +2,25 @@ package com.shopu.shopu.Activities.DetalhesProduto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.shopu.shopu.R
+import com.shopu.shopu.databinding.ActivityDetailProdutosBinding
 
 class detail_produtos : AppCompatActivity() {
+
+    lateinit var biding: ActivityDetailProdutosBinding// para inflar os produtos da lista para os detalhes
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_produtos)
+        biding = ActivityDetailProdutosBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+
+        val foto = intent.extras?.getString("foto")// para recuperar os dados no adapter tela principal para os detalhes
+        val nome = intent.extras?.getString("nome")
+        val preco = intent.extras?.getString("preco")
+
+        Glide.with(this).load(foto).into(biding.fotoTenis)
+        biding.nomeProduto.text = nome
+        biding.preco.text = "R$ ${preco}" // fomos na activity detalhes do produto e colocamos os seguintes id para fazer a recuperacao exata
     }
 }
