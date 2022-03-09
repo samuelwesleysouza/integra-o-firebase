268d768edd1cc545be05db5e1263c33e953a97eb
samue <Samuka36>
2022-03-09 10:18:48 -0300

    Tela de pagamento junto com as validações das numerações e pedido da tela de detail

--- app/src/main/java/com/shopu/shopu/Activities/DetalhesProduto/detail_produtos.kt
+++ app/src/main/java/com/shopu/shopu/Activities/DetalhesProduto/detail_produtos.kt
@@ -1,12 +1,67 @@
-package com.shopu.shopu.Activities.DetalhesProduto
+package com.shopu.shopu
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.shopu.shopu.R
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.shopu.shopu.Activities.Pagamento.Pagamento
+import com.shopu.shopu.databinding.ActivityDetailProdutosBinding
 
 class detail_produtos : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailProdutosBinding
+    var tamanho_calcado = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_produtos)
+        setContentView(binding.root)
+
+        val foto = intent.extras?.getString("foto")
+        val nome = intent.extras?.getString("nome")
+        val preco = intent.extras?.getString("preco")
+
+        Glide.with(this).load(foto).into(binding.fotoTenis)
+        binding.nomeProduto.text = nome
+        binding.preco.text = "R$ ${preco}"
+
+        binding.botaoFinalizarpedido.setOnClickListener {
+
+            val tamanho_calcado_38 = binding.tamanho38
+            val tamanho_calcado_39 = binding.tamanho39
+            val tamanho_calcado_40 = binding.tamanho40
+            val tamanho_calcado_41 = binding.tamanho41
+            val tamanho_calcado_42 = binding.tamanho42
+
+            when (true) {
+                tamanho_calcado_38.isChecked -> tamanho_calcado = "38"
+                tamanho_calcado_39.isChecked -> tamanho_calcado = "39"
+                tamanho_calcado_40.isChecked -> tamanho_calcado = "40"
+                tamanho_calcado_41.isChecked -> tamanho_calcado = "41"
+                tamanho_calcado_42.isChecked -> tamanho_calcado = "42"
+            }
+
+            if (!tamanho_calcado_38.isChecked &&
+                !tamanho_calcado_39.isChecked &&
+                !tamanho_calcado_40.isChecked &&
+                !tamanho_calcado_41.isChecked &&
+                !tamanho_calcado_42.isChecked
+            ) {
+
+                val snackbar =
+                    Snackbar.make(it, "Escolha o tamanho do calçado!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.setTextColor(Color.WHITE)
+                snackbar.show()
+            } else {
+                val intent = Intent(this, Pagamento::class.java)
+                intent.putExtra("tamano_calcado", tamanho_calcado)
+                intent.putExtra("nome", nome)
+                intent.putExtra("preco", preco)
+                startActivity(intent)
+            }
+        }
     }
 }
